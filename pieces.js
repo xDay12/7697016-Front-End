84758d60999cdc35c3b774b98d8240219ca61725
Vincent Ricard <vince-0209@hotmail.fr>
2023-01-30 13:47:22 +0100

    sauvegarde 4

--- pieces.js
+++ pieces.js
@@ -3,32 +3,42 @@ const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
 // Création des balises
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+    const article = pieces[i];
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d'une balise dédiée à une piece automobile
+    const pieceElement = document.createElement("article");
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    // Création des balises
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-// Rattachement des nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l'image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+
+}
